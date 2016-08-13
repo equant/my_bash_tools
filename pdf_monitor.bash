c7091dd7819f2ca05907e7f70e43a6a00c561125
equant <nathanhendler@gmail.com>
2016-08-12 21:13:49 -0700

    wtf 8 to 9

--- pdf_monitor.bash
+++ pdf_monitor.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-pdfjam `find . -type f -name '*pdf' -printf '%T@ %p\n' | sort -n | tail -8 | cut -f2- -d" "` --nup 3x3 --landscape --outfile ~/tmp/monitor.pdf
+pdfjam `find . -type f -name '*pdf' -printf '%T@ %p\n' | sort -n | tail -9 | cut -f2- -d" "` --nup 3x3 --landscape --outfile ~/tmp/monitor.pdf
 zathura ~/tmp/monitor.pdf &
 inotifywait --recursive --monitor -e close_write,moved_to,create,delete  . |
 while read -r directory events filename; do
