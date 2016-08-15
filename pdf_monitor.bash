9b9061aa225ef954d2ca26ac30bc7e735c0ffb39
Nathanial Hendler <equant@lpl.arizona.edu>
2016-08-15 14:14:10 -0700

    Faster better duplicate detection (assumed you won't have more than 3-4 dupes) and only uses first page of a pdf in monitor output.

--- pdf_monitor.bash
+++ pdf_monitor.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-pdfjam `find . -type f -name '*.pdf' -printf '%T@ %p\n' | sort -n | while read x; do awk '{printf($2 " "); system("od " $2 " 2> /dev/null| head -200 | sha1sum")}'; done | sort -u -k2 | awk '{print($1)}' | tail -9 | cut -f2- -d" "` --nup 3x3 --landscape --outfile ~/tmp/monitor.pdf
+pdfjam `find . -type f -name '*.pdf' -printf '%T@ %p\n' | sort -rn | head -50 | while read x; do echo "$x" | awk '{printf($2 " "); system("od " $2 " 2> /dev/null| head -200 | sha1sum")}'; done | awk '!a[$2]++' | awk '{print $1}' | head -9` 1 --nup 3x3 --outfile ~/tmp/monitor.pdf
 zathura ~/tmp/monitor.pdf &
 inotifywait --recursive --monitor -e close_write,moved_to,create,delete  . |
 while read -r directory events filename; do
   if [[ $filename == *pdf ]]; then
-pdfjam `find . -type f -name '*.pdf' -printf '%T@ %p\n' | sort -n | while read x; do awk '{printf($2 " "); system("od " $2 " 2> /dev/null| head -200 | sha1sum")}'; done | sort -u -k2 | awk '{print($1)}' | tail -9 | cut -f2- -d" "` --nup 3x3 --landscape --outfile ~/tmp/monitor.pdf
+      pdfjam `find . -type f -name '*.pdf' -printf '%T@ %p\n' | sort -rn | head -50 | while read x; do echo "$x" | awk '{printf($2 " "); system("od " $2 " 2> /dev/null| head -200 | sha1sum")}'; done | awk '!a[$2]++' | awk '{print $1}' | head -9` 1 --nup 3x3 --outfile ~/tmp/monitor.pdf
   fi
 done
