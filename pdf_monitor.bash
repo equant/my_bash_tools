f75476ebb68b6719ffe624317f7ade5e7410443e
Nathanial Hendler <equant@lpl.arizona.edu>
2016-08-16 12:38:03 -0700

    8 pages of sweetness!

--- pdf_monitor.bash
+++ pdf_monitor.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-pdfjam `find . -type f -name '*.pdf' -printf '%T@ %p\n' | sort -rn | head -50 | while read x; do echo "$x" | awk '{printf($2 " "); system("od " $2 " 2> /dev/null| head -200 | sha1sum")}'; done | awk '!a[$2]++' | awk '{print $1}' | head -9` 1 --nup 3x3 --outfile ~/tmp/monitor.pdf
+pdfjam `find . -type f -name '*.pdf' -printf '%T@ %p\n' | sort -rn | head -100 | while read x; do echo "$x" | awk '{printf($2 " "); system("od " $2 " 2> /dev/null| head -200 | sha1sum")}'; done | awk '!a[$2]++' | awk '{print $1}' | head -32` 1 --nup 2x2 --outfile ~/tmp/monitor.pdf
 zathura ~/tmp/monitor.pdf &
 inotifywait --recursive --monitor -e close_write,moved_to,create,delete  . |
 while read -r directory events filename; do
   if [[ $filename == *pdf ]]; then
-      pdfjam `find . -type f -name '*.pdf' -printf '%T@ %p\n' | sort -rn | head -50 | while read x; do echo "$x" | awk '{printf($2 " "); system("od " $2 " 2> /dev/null| head -200 | sha1sum")}'; done | awk '!a[$2]++' | awk '{print $1}' | head -9` 1 --nup 3x3 --outfile ~/tmp/monitor.pdf
+      pdfjam `find . -type f -name '*.pdf' -printf '%T@ %p\n' | sort -rn | head -100 | while read x; do echo "$x" | awk '{printf($2 " "); system("od " $2 " 2> /dev/null| head -200 | sha1sum")}'; done | awk '!a[$2]++' | awk '{print $1}' | head -32` 1 --nup 2x2 --outfile ~/tmp/monitor.pdf
   fi
 done
