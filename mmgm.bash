857c3671cad309637488ebc6c72ecefba57f4e33
Nathanial Hendler <nathanhendler@gmail.com>
2016-11-11 14:24:16 -0700

    mmgm: bash functions for bookmarking (mm) and cd'ing to (gm) directories

---
+++ mmgm.bash
@@ -0,0 +1,45 @@
+function mm() {
+    CONF_DIR="~/.config/mmgm"
+    USAGE="Usage: mm [bookmark]" ;
+
+    if  [ ! -d ~/.config/mmgm ] ; then
+        mkdir -p ~/.config/mmgm
+    fi
+
+    if [ $# -eq 0 ] ; then
+		# number of arguments is zero
+        echo "cd `pwd`" > ~/.config/mmgm/m;
+    else
+        case $1 in
+            -h)
+                echo "$USAGE";
+                ;;
+            *)
+                echo "cd `pwd`" > ~/.config/mmgm/"$1";
+                ;;
+        esac
+    fi
+}
+
+function gm() {
+    CONF_DIR="~/.config/mmgm"
+    USAGE="Usage: gm [bookmark]" ;
+
+    if  [ ! -d ~/.config/mmgm ] ; then
+        echo "You have no bookmarks"
+    fi
+
+    if [ $# -eq 0 ] ; then
+		# number of arguments is zero
+        source ~/.config/mmgm/m;
+    else
+        case $1 in
+            -h)
+                echo "$USAGE";
+                ;;
+            *)
+                source ~/.config/mmgm/"$1";
+                ;;
+        esac
+    fi
+}
